7de09396e9b91f80399a5a474403e8fa1ab30810
Asif Ahsan <56015056+AsifAhsan4980@users.noreply.github.com>
2021-09-16 21:41:22 +0600

    Book sell

--- app/src/main/java/com/example/onlybooksapp/LoginActivity.java
+++ app/src/main/java/com/example/onlybooksapp/LoginActivity.java
@@ -2,26 +2,38 @@ package com.example.onlybooksapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextInputEditText etLoginEmail;
     TextInputEditText etLoginPassword;
-    TextView tvRegisterHere;
+    TextView tvRegisterHere, set_location;
     Button btnLogin;
+
 
     FirebaseAuth mAuth;
 
@@ -35,7 +47,14 @@ public class LoginActivity extends AppCompatActivity {
         tvRegisterHere = findViewById(R.id.tvRegisterHere);
         btnLogin = findViewById(R.id.btnLogin);
 
+
         mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
 
         btnLogin.setOnClickListener(view -> {
             loginUser();
@@ -61,7 +80,7 @@ public class LoginActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
                         Toast.makeText(LoginActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(LoginActivity.this, AddbookActivity.class));
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     }else{
                         Toast.makeText(LoginActivity.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
@@ -70,4 +89,89 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    public static class BorrowActivity extends AppCompatActivity
+    {
+        RecyclerView recview;
+        myadapter adapter;
+        FloatingActionButton fb;
+        Switch borrow_buy;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_borrow);
+            recview=(RecyclerView)findViewById(R.id.recview);
+            recview.setLayoutManager(new LinearLayoutManager(this));
+
+            FirebaseRecyclerOptions<model> options =
+                    new FirebaseRecyclerOptions.Builder<model>()
+                            .setQuery(FirebaseDatabase.getInstance("https://onlybooks-e1483-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("user"), model.class)
+                            .build();
+
+            adapter=new myadapter(options);
+            recview.setAdapter(adapter);
+
+            borrow_buy= (Switch) findViewById(R.id.borrow_buy);
+            borrow_buy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            adapter.startListening();
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            adapter.stopListening();
+        }
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            getMenuInflater().inflate(R.menu.searchmenu,menu);
+
+            MenuItem item=menu.findItem(R.id.search);
+
+            SearchView searchView=(SearchView)item.getActionView();
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+            {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+
+                    processsearch(s);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    processsearch(s);
+                    return false;
+                }
+            });
+
+            return super.onCreateOptionsMenu(menu);
+        }
+
+        private void processsearch(String s)
+        {
+            FirebaseRecyclerOptions<model> options =
+                    new FirebaseRecyclerOptions.Builder<model>()
+                            .setQuery(FirebaseDatabase.getInstance("https://onlybooks-e1483-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("user").orderByChild("name").startAt(s).endAt(s+"\uf8ff"), model.class)
+                            .build();
+
+            adapter=new myadapter(options);
+            adapter.startListening();
+            recview.setAdapter(adapter);
+
+        }
+    }
 }
--- app/src/main/java/com/example/onlybooksapp/MainActivity.java
+++ app/src/main/java/com/example/onlybooksapp/MainActivity.java
@@ -1,14 +1,132 @@
 package com.example.onlybooksapp;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class MainActivity extends AppCompatActivity
+{
+    RecyclerView recview;
+    myadapter adapter;
+    FloatingActionButton fb;
+    Switch borrow_buy;
+    Button log_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Search here..");
+
+        recview=(RecyclerView)findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance("https://onlybooks-e1483-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("user"), model.class)
+                        .build();
+
+        adapter=new myadapter(options);
+        recview.setAdapter(adapter);
+
+        fb=(FloatingActionButton)findViewById(R.id.fadd);
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),AddbookActivity.class));
+            }
+        });
+        borrow_buy= (Switch) findViewById(R.id.borrow_buy);
+        borrow_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        log_out= findViewById(R.id.logout);
+
+        log_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.searchmenu,menu);
+
+        MenuItem item=menu.findItem(R.id.search);
+
+        SearchView searchView=(SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                processsearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                processsearch(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void processsearch(String s)
+    {
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance("https://onlybooks-e1483-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("user").orderByChild("name").startAt(s).endAt(s+"\uf8ff"), model.class)
+                        .build();
+
+        adapter=new myadapter(options);
+        adapter.startListening();
+        recview.setAdapter(adapter);
+
     }
 }
